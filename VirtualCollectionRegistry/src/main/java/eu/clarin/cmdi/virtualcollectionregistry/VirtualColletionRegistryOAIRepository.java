cb45162ddf012d86baf304aa41240edbd83cc311
Oliver Schonefeld <schonefeld@ids-mannheim.de>
2010-06-16 16:28:23 +0000

    - generate queries using criteria api

--- VirtualCollectionRegistry/src/main/java/eu/clarin/cmdi/virtualcollectionregistry/VirtualColletionRegistryOAIRepository.java
+++ VirtualCollectionRegistry/src/main/java/eu/clarin/cmdi/virtualcollectionregistry/VirtualColletionRegistryOAIRepository.java
@@ -7,11 +7,18 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import eu.clarin.cmdi.virtualcollectionregistry.model.VirtualCollection;
-import eu.clarin.cmdi.virtualcollectionregistry.model.VirtualCollectionList;
+import eu.clarin.cmdi.virtualcollectionregistry.model.VirtualCollection_;
 import eu.clarin.cmdi.virtualcollectionregistry.oai.OAIException;
 import eu.clarin.cmdi.virtualcollectionregistry.oai.OAIOutputStream;
 import eu.clarin.cmdi.virtualcollectionregistry.oai.OAIOutputStream.NamespaceDecl;
@@ -153,7 +160,7 @@ class VirtualColletionRegistryOAIRepository implements OAIRepository {
 
     @Override
     public DeletedNotion getDeletedNotion() {
-        return DeletedNotion.NO;
+        return DeletedNotion.TRANSIENT;
     }
 
     @Override
@@ -231,23 +238,69 @@ class VirtualColletionRegistryOAIRepository implements OAIRepository {
     public RecordList getRecords(String prefix, Date from, Date until,
             String set, int offset, boolean headerOnly) throws OAIException {
         try {
-            VirtualCollectionList results =
-                registry.getVirtualCollections(null, offset, 2);
-            List<VirtualCollection> vcs = results.getItems();
-            if (!vcs.isEmpty()) {
+            EntityManager em = registry.getDataStore().getEntityManager();
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+
+            CriteriaQuery<Long> cq1 = cb.createQuery(Long.class);
+            Root<VirtualCollection> root1 = cq1.from(VirtualCollection.class);
+            cq1.select(cb.count(root1));
+            cq1.where(buildWhere(cb, cq1, root1, from, until));
+
+            CriteriaQuery<VirtualCollection> cq2 =
+                cb.createQuery(VirtualCollection.class);
+            Root<VirtualCollection> root2 = cq2.from(VirtualCollection.class);
+            cq2.select(root2);
+            cq2.where(buildWhere(cb, cq2, root2, from, until));
+            cq2.orderBy(cb.asc(root2.get(VirtualCollection_.modifedDate)));
+            
+            TypedQuery<Long> q1 = em.createQuery(cq1);
+            TypedQuery<VirtualCollection> q2 = em.createQuery(cq2);
+            
+            em.getTransaction().begin();
+            List<VirtualCollection> vcs = null;
+            long count = q1.getSingleResult();
+            if (count > 0) {
+                if (offset > 0) {
+                    q2.setFirstResult(offset);
+                }
+                q2.setMaxResults(100);
+                vcs = q2.getResultList();
+            }
+            em.getTransaction().commit();
+
+            if ((vcs != null) && !vcs.isEmpty()) {
                 List<Record> records = new ArrayList<Record>(vcs.size());
                 for (VirtualCollection vc : vcs) {
                     records.add(createRecord(vc, headerOnly));
                 }
-                boolean hasMore = ((results.getOffset() + vcs.size()) < results
-                        .getTotalCount());
-                return new RecordList(records, results.getOffset(), hasMore,
-                        results.getTotalCount());
+                boolean hasMore = (offset + vcs.size()) < count;
+                return new RecordList(records, offset, hasMore, (int) count);
+            } else {
+                return null;
             }
-            return null;
-        } catch (VirtualCollectionRegistryException e) {
-            throw new OAIException("error", e);
+        } catch (Exception e) {
+            throw new OAIException("error fetcing records", e);
+        } finally {
+            registry.getDataStore().closeEntityManager();
         }
+    }
+
+    private Predicate buildWhere(CriteriaBuilder cb, CriteriaQuery<?> cq,
+            Root<VirtualCollection> root, Date from, Date until) {
+        Predicate where =
+                cb.equal(root.get(VirtualCollection_.state),
+                                  VirtualCollection.State.PUBLIC);
+        if ((from != null) && (until != null)) {
+            where = cb.and(where, cb.between(root
+                    .get(VirtualCollection_.modifedDate), from, until));
+        } else if (from != null) {
+            where = cb.and(where, cb.greaterThanOrEqualTo(root
+                    .get(VirtualCollection_.modifedDate), from));
+        } else if (until != null) {
+            where = cb.and(where, cb.lessThanOrEqualTo(root
+                    .get(VirtualCollection_.modifedDate), until));
+        }
+        return where;
     }
 
     private static class RecordFullImpl implements Record {
@@ -269,8 +322,7 @@ class VirtualColletionRegistryOAIRepository implements OAIRepository {
 
         @Override
         public boolean isDeleted() {
-            // FIXME: check vc state
-            return false;
+            return vc.getState() == VirtualCollection.State.DELETED;
         }
 
         @Override
@@ -298,10 +350,12 @@ class VirtualColletionRegistryOAIRepository implements OAIRepository {
     private static class RecordHeaderImpl implements Record {
         final Long id;
         final Date datestamp;
+        final boolean deleted;
 
         RecordHeaderImpl(VirtualCollection vc) {
             this.id = vc.getId();
             this.datestamp = vc.getModifiedDate();
+            this.deleted = (vc.getState() == VirtualCollection.State.DELETED);
         }
 
         @Override
@@ -316,8 +370,7 @@ class VirtualColletionRegistryOAIRepository implements OAIRepository {
 
         @Override
         public boolean isDeleted() {
-            // FIXME: check vc state
-            return false;
+            return deleted;
         }
 
         @Override
