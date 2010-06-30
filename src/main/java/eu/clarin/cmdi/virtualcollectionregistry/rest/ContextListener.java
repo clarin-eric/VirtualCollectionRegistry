9702bb5ae98c9c747e7cbb9a44509162fc6b1d44
Oliver Schonefeld <schonefeld@ids-mannheim.de>
2010-06-30 21:58:45 +0000

    - revert r558

--- src/main/java/eu/clarin/cmdi/virtualcollectionregistry/rest/ContextListener.java
+++ src/main/java/eu/clarin/cmdi/virtualcollectionregistry/rest/ContextListener.java
@@ -10,34 +10,34 @@ import javax.servlet.ServletContextListener;
 import eu.clarin.cmdi.virtualcollectionregistry.VirtualCollectionRegistry;
 import eu.clarin.cmdi.virtualcollectionregistry.VirtualCollectionRegistryException;
 
+
 public class ContextListener implements ServletContextListener {
 
-    @SuppressWarnings("rawtypes")
-    public void contextInitialized(ServletContextEvent event) {
-        ServletContext ctx = event.getServletContext();
-        HashMap<String, String> config = new HashMap<String, String>();
-        for (Enumeration i = ctx.getInitParameterNames();
-             i.hasMoreElements();) {
-            String key = (String) i.nextElement();
-            String value = ctx.getInitParameter(key);
-            if ((value != null) && (value.length() > 0)) {
-                config.put(key, value);
-            }
-        }
-        try {
-            VirtualCollectionRegistry.initalize(config);
-        } catch (VirtualCollectionRegistryException e) {
-            ctx.log("error initializing registry", e);
-        }
-    }
+	@SuppressWarnings("unchecked")
+	public void contextInitialized(ServletContextEvent event) {
+		ServletContext ctx = event.getServletContext();
+		HashMap<String, String> config = new HashMap<String, String>();
+		for (Enumeration i = ctx.getInitParameterNames(); i.hasMoreElements();) {
+			String key   = (String) i.nextElement();
+			String value = ctx.getInitParameter(key);
+			if ((value != null) && (value.length() > 0)) {
+				config.put(key, value);
+			}
+		}
+		try {
+			VirtualCollectionRegistry.initalize(config);
+		} catch (VirtualCollectionRegistryException e) {
+			ctx.log("error initializing registry", e);
+		}
+	}
 
-    public void contextDestroyed(ServletContextEvent event) {
-        ServletContext ctx = event.getServletContext();
-        try {
-            VirtualCollectionRegistry.instance().destroy();
-        } catch (VirtualCollectionRegistryException e) {
-            ctx.log("error destroying registry", e);
-        }
-    }
+	public void contextDestroyed(ServletContextEvent event) {
+		ServletContext ctx = event.getServletContext();
+		try {
+			VirtualCollectionRegistry.instance().destroy();
+		} catch (VirtualCollectionRegistryException e) {
+			ctx.log("error destroying registry", e);
+		}
+	}
 
 } // class ContextListener
