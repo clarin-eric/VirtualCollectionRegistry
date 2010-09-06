7542a11db153f82e70485138f332bc4e9a39bc96
Oliver Schonefeld <schonefeld@ids-mannheim.de>
2010-09-06 15:33:33 +0000

    - remove more sophisticated xs:any stuff from GeneretedBy element in XSD - map Query in GeneratedBy element to String for now - some minor style updates in XSD

--- src/main/java/eu/clarin/cmdi/virtualcollectionregistry/model/GeneratedBy.java
+++ src/main/java/eu/clarin/cmdi/virtualcollectionregistry/model/GeneratedBy.java
@@ -2,20 +2,54 @@ package eu.clarin.cmdi.virtualcollectionregistry.model;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+import javax.persistence.Lob;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
 
 @Embeddable
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(propOrder = { "description", "query" },
          namespace = "urn:x-vcr:generatedby")
+@XmlSeeAlso(GeneratedBy.Query.class)
 public class GeneratedBy {
+    @Embeddable
+    @XmlAccessorType(XmlAccessType.NONE)
+    public static class Query {
+        @Column(name = "generatedby_query_profile")
+        @XmlAttribute(name = "profile", required = true)
+        private String profile;
+        @Column(name = "generatedby_query_value")
+        @Lob
+        @XmlValue
+        private String value;
+
+        public void setProfile(String profile) {
+            this.profile = profile;
+        }
+
+        public String getProfile() {
+            return profile;
+        }
+
+        public void setQuery(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    } // class Query
+
     @Column(name = "generatedby_description")
     private String description;
-    @Column(name = "generatedby_query")
-    private String query;
+    @Embedded
+    private Query query;
 
     public GeneratedBy() {
         super();
@@ -30,12 +64,12 @@ public class GeneratedBy {
         return description;
     }
 
-    public void setQuery(String query) {
+    public void setQuery(Query query) {
         this.query = query;
     }
 
     @XmlElement(name = "Query")
-    public String getQuery() {
+    public Query getQuery() {
         return query;
     }
 
