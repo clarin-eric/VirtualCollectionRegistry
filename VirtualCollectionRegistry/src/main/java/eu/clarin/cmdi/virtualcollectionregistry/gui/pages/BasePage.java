b5026793db80335c82a3d4647e7cd715b83341fe
Oliver Schonefeld <schonefeld@ids-mannheim.de>
2010-11-11 14:14:31 +0000

    - add role based authorization - add external authentication filters (HTTP Basic Auth, Shibboleth - add stub Admin page

--- VirtualCollectionRegistry/src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/pages/BasePage.java
+++ VirtualCollectionRegistry/src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/pages/BasePage.java
@@ -1,14 +1,20 @@
 package eu.clarin.cmdi.virtualcollectionregistry.gui.pages;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
 
 import eu.clarin.cmdi.virtualcollectionregistry.VirtualCollectionRegistry;
+import eu.clarin.cmdi.virtualcollectionregistry.gui.ApplicationSession;
 
 public class BasePage extends WebPage {
 
-    public BasePage() {
+    protected BasePage() {
         super();
         // authentication state
         add(new AuthenticationStatePanel("authstate"));
@@ -21,11 +27,47 @@ public class BasePage extends WebPage {
         menu.addMenuItem(new MenuItem<CreateVirtualCollectionPage>(
                 new Model<String>("Create Virtual Collection"),
                 CreateVirtualCollectionPage.class));
+        menu.addMenuItem(new MenuItem<AdminPage>(
+                new Model<String>("Admin Page"),
+                AdminPage.class));
         add(menu);
         
         // add version to footer
         VirtualCollectionRegistry vcr = VirtualCollectionRegistry.instance();
         add(new Label("version", vcr.getVersion()).setRenderBodyOnly(true));
+    }
+
+    @Override
+    protected void onBeforeRender() {
+        // skip lazy auto-auth for login page
+        if (!this.getClass().isInstance(LoginPage.class)) {
+            final HttpServletRequest request =
+                getWebRequestCycle().getWebRequest().getHttpServletRequest();
+            final ApplicationSession session =
+                (ApplicationSession) getSession();
+            if (!session.isSignedIn()) {
+                if (request.getAuthType() != null) {
+                    // FIXME: better logging
+                    System.err.println("Auth, but no authed session -> login");
+                    final Principal principal = request.getUserPrincipal();
+                    if (!session.signIn(principal)) {
+                        throw new RestartResponseException(getApplication()
+                                .getApplicationSettings()
+                                .getAccessDeniedPage());
+                    }
+                }
+            } else {
+                if (request.getAuthType() == null) {
+                    // FIXME: better logging
+                    System.err.println("Lost Session!");
+                    session.invalidate();
+                    throw new RestartResponseException(getApplication()
+                            .getApplicationSettings()
+                            .getPageExpiredErrorPage());
+                }
+            }
+        }
+        super.onBeforeRender();
     }
 
 } // class BasePage
