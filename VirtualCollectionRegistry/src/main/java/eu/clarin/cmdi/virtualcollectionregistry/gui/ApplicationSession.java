3b67ca07c0e56f1143391bf7eb1b05938ccd1acf
Oliver Schonefeld <schonefeld@ids-mannheim.de>
2010-11-26 17:12:05 +0000

    - work around for users where only the persistent-id attribute is available

--- VirtualCollectionRegistry/src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/ApplicationSession.java
+++ VirtualCollectionRegistry/src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/ApplicationSession.java
@@ -1,20 +1,28 @@
 package eu.clarin.cmdi.virtualcollectionregistry.gui;
 
 import java.security.Principal;
+import java.util.regex.Pattern;
 
 import org.apache.wicket.Request;
 import org.apache.wicket.authentication.AuthenticatedWebSession;
 import org.apache.wicket.authorization.strategies.role.Roles;
 
+import eu.clarin.cmdi.virtualcollectionregistry.gui.auth.AuthPrincipal;
+
 
 @SuppressWarnings("serial")
 public class ApplicationSession extends AuthenticatedWebSession {
+    private static final String[] ATTRIBUTE_NAMES_NAME =
+        { "cn", "commonName", "displayName" };
+    private static final Pattern PERSITENT_ID_REGEX =
+        Pattern.compile("^[^!]+![^!]+![^!]+$");
     private static final Roles ROLES_USER =
         new Roles(Roles.USER);
     private static final Roles ROLES_ADMIN =
         new Roles(new String[] { Roles.USER, Roles.ADMIN}); 
-    private Principal principal;
+    private String user;
     private boolean isAdmin;
+    private String userDisplay;
     
     public ApplicationSession(Request request) {
         super(request);
@@ -25,10 +33,9 @@ public class ApplicationSession extends AuthenticatedWebSession {
         if (principal != null) {
             result = signIn(principal.getName(), null);
             if (result) {
-                // XXX: possibly do something with attributes
-                this.principal = principal; 
-                this.isAdmin =
-                    ((Application) getApplication()).isAdmin(getUser());
+                user = principal.getName();
+                isAdmin = ((Application) getApplication()).isAdmin(user);
+                userDisplay = findDisplayName(principal);
             }
         }
         return result; 
@@ -52,15 +59,48 @@ public class ApplicationSession extends AuthenticatedWebSession {
     }
 
     public Principal getPrincipal() {
-        return principal;
+        return new Principal() {
+            @Override
+            public String getName() {
+                return user;
+            }
+        };
     }
 
     public String getUser() {
-        return principal.getName();
+        return user;
+    }
+
+    public String getUserDisplay() {
+        if (userDisplay != null) {
+            return userDisplay;
+        }
+        if (PERSITENT_ID_REGEX.matcher(user).matches()) {
+            return "Authenticated via Shibboleth";
+        }
+        return user;
     }
 
     public static ApplicationSession get() {
         return (ApplicationSession) AuthenticatedWebSession.get();
+    }
+
+    private static String findDisplayName(Principal p) {
+        if (p instanceof AuthPrincipal) {
+            final AuthPrincipal principal = (AuthPrincipal) p;
+            for (String attr : ATTRIBUTE_NAMES_NAME ) {
+                String name = principal.getAttibute(attr);
+                if (name != null) {
+                    return name;
+                }
+            }
+            String givenName = principal.getAttibute("givenName");
+            String surname = principal.getAttibute("surname");
+            if ((givenName != null) && (surname != null)) {
+                return givenName + " " + surname;
+            }
+        }
+        return null;
     }
 
 } // class ApplicationSession
