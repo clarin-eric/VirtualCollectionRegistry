ac333bf787a6a73217bcda18688e17f3bddf8798
Oliver Schonefeld <schonefeld@ids-mannheim.de>
2010-12-09 14:52:09 +0000

    - use WiQuery plugin - clean up dependencies

--- src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/border/AjaxToggleBorder.java
+++ src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/border/AjaxToggleBorder.java
@@ -4,29 +4,51 @@ import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.border.Border;
-import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.odlabs.wiquery.core.effects.EffectSpeed;
+import org.odlabs.wiquery.core.effects.sliding.SlideToggle;
+import org.odlabs.wiquery.core.events.Event;
+import org.odlabs.wiquery.core.events.MouseEvent;
+import org.odlabs.wiquery.core.events.WiQueryEventBehavior;
+import org.odlabs.wiquery.core.javascript.JsScope;
+import org.odlabs.wiquery.core.javascript.JsStatement;
 
 @SuppressWarnings("serial")
 public class AjaxToggleBorder extends Border {
-
-    public AjaxToggleBorder(String id, IModel<String> title, final boolean expanded) {
+    private final static String COLLAPSED_CLASS = "collapsed";
+    
+    public AjaxToggleBorder(String id, IModel<String> title,
+            final boolean expanded) {
         super(id);
         setRenderBodyOnly(true);
 
         final WebMarkupContainer header = new WebMarkupContainer("header");
         header.add(new Label("title", title));
-        header.add(new AttributeAppender("class",
-                new AbstractReadOnlyModel<String>() {
-                    @Override
-                    public String getObject() {
-                        return expanded ? "shown" : "hidden";
-                    }
-                }, " "));
-        add(header);
+
         final WebMarkupContainer content = new WebMarkupContainer("content");
+        content.setOutputMarkupId(true);
         content.add(getBodyContainer());
+        add(header);
         add(content);
+
+        header.add(new WiQueryEventBehavior(new Event(MouseEvent.CLICK) {
+            @Override
+            public JsScope callback() {
+                final JsScope cb = JsScope.quickScope(new JsStatement()
+                    .$(header).toggleClass(COLLAPSED_CLASS));
+                final SlideToggle effect = new SlideToggle(EffectSpeed.SLOW);
+                effect.setCallback(cb);
+                return JsScope.quickScope(new JsStatement()
+                    .$(content).chain(effect));
+            }
+        }));
+        if (!expanded) {
+            header.add(new AttributeAppender("class",
+                    new Model<String>(COLLAPSED_CLASS), " "));
+            content.add(new AttributeAppender("style",
+                    new Model<String>("display:none"), ";"));
+        }
     }
 
     public AjaxToggleBorder(String id, IModel<String> title) {
