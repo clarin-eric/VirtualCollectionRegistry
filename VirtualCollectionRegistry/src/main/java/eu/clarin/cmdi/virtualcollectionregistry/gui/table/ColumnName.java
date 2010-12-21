5656bfc1f4bc7a40a9371dcb9f8cfdff8a0ac664
Oliver Schonefeld <schonefeld@ids-mannheim.de>
2010-12-21 19:35:08 +0000

    - use IWiQueryPlugin for jQuery enabled components - move JavaScript code to separate files

--- VirtualCollectionRegistry/src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/table/ColumnName.java
+++ VirtualCollectionRegistry/src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/table/ColumnName.java
@@ -1,5 +1,6 @@
 package eu.clarin.cmdi.virtualcollectionregistry.gui.table;
 
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
@@ -11,53 +12,54 @@ import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
-import org.odlabs.wiquery.core.effects.EffectSpeed;
-import org.odlabs.wiquery.core.effects.sliding.SlideToggle;
-import org.odlabs.wiquery.core.events.Event;
-import org.odlabs.wiquery.core.events.MouseEvent;
-import org.odlabs.wiquery.core.events.WiQueryEventBehavior;
-import org.odlabs.wiquery.core.javascript.JsScope;
+import org.odlabs.wiquery.core.commons.IWiQueryPlugin;
+import org.odlabs.wiquery.core.commons.WiQueryResourceManager;
 import org.odlabs.wiquery.core.javascript.JsStatement;
 
 import eu.clarin.cmdi.virtualcollectionregistry.model.VirtualCollection;
 
 @SuppressWarnings("serial")
 final class ColumnName extends AbstractColumn<VirtualCollection> {
-    private final class ItemCell extends Panel {
-        private final static String DETAILS_SHOWN_CLASS = "detailsShown";
+    private static final ResourceReference JAVASCRIPT_RESOURCE = 
+        new ResourceReference(ColumnName.class, "ColumnName.js");
 
+    private final class ItemCell extends Panel implements IWiQueryPlugin {
+        private final WebMarkupContainer nameColumn;
+        
         public ItemCell(String id, IModel<VirtualCollection> model) {
             super(id);
             setRenderBodyOnly(true);
-            final VirtualCollection vc = model.getObject();
-            final Label nameLabel = new Label("name", vc.getName());
-            add(nameLabel);
             
+            nameColumn = new WebMarkupContainer("nameColumn");
+            nameColumn.setOutputMarkupId(true);
+            final VirtualCollection vc = model.getObject();
+            nameColumn.add(new Label("name", vc.getName()));
+
             final WebMarkupContainer details =
                 new WebMarkupContainer("details");
-            details.setOutputMarkupId(true);
-            
             final String desc = vc.getDescription();
             final MultiLineLabel descLabel = new MultiLineLabel("desc", desc);
             if (desc == null) {
                 descLabel.setVisible(false);
             }
             details.add(descLabel);
-            add(details);
-            nameLabel.add(new WiQueryEventBehavior(new Event(MouseEvent.CLICK) {
-                @Override
-                public JsScope callback() {
-                    final JsScope cb = JsScope.quickScope(new JsStatement()
-                        .$(nameLabel).toggleClass(DETAILS_SHOWN_CLASS));
-                    final SlideToggle effect = new SlideToggle(EffectSpeed.SLOW);
-                    effect.setCallback(cb);
-                    return JsScope.quickScope(new JsStatement()
-                        .$(details).chain(effect));
-                }
-            }));
+            // move to css?
             details.add(new AttributeAppender("style",
                     new Model<String>("display:none"), ";"));
+            nameColumn.add(details);
+            add(nameColumn);
         }
+        
+        @Override
+        public void contribute(WiQueryResourceManager manager) {
+            manager.addJavaScriptResource(JAVASCRIPT_RESOURCE);
+        }
+
+        @Override
+        public JsStatement statement() {
+            return new JsStatement().$(nameColumn).append(".detailsToggle()");
+        }
+
     } // class ColumnName.ItemCell
 
     ColumnName(VirtualCollectionTable table) {
