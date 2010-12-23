2701fcc6bbcd6a60ab287172b774305a24a01c86
Oliver Schonefeld <schonefeld@ids-mannheim.de>
2010-12-23 14:43:21 +0000

    - move translations for virtual collection enum types to Application.properties - show correct (human readable) values to enum types on virtual collections details page - add CSS to virtual collections details page

--- src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/pages/VirtualCollectionDetailsPage.java
+++ src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/pages/VirtualCollectionDetailsPage.java
@@ -1,19 +1,24 @@
 package eu.clarin.cmdi.virtualcollectionregistry.gui.pages;
 
-import java.util.ArrayList;
+import java.sql.Date;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
+import org.apache.commons.lang.time.FastDateFormat;
+import org.apache.wicket.Application;
 import org.apache.wicket.Component;
 import org.apache.wicket.Page;
 import org.apache.wicket.behavior.AbstractBehavior;
 import org.apache.wicket.extensions.ajax.markup.html.repeater.data.table.AjaxFallbackDefaultDataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.basic.MultiLineLabel;
 import org.apache.wicket.markup.html.border.Border;
@@ -22,10 +27,14 @@ import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.link.PopupSettings;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.list.OddEvenListItem;
+import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.ComponentPropertyModel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.util.convert.IConverter;
+import org.apache.wicket.util.string.Strings;
 
 import eu.clarin.cmdi.virtualcollectionregistry.gui.border.AjaxToggleBorder;
 import eu.clarin.cmdi.virtualcollectionregistry.model.Creator;
@@ -36,7 +45,36 @@ import eu.clarin.cmdi.virtualcollectionregistry.model.VirtualCollection.Type;
 
 @SuppressWarnings("serial")
 public class VirtualCollectionDetailsPage extends BasePage {
-    private static final String CSS_CLASS = "collectionsDetails";
+    private static final IConverter convEnum = new IConverter() {
+        @Override
+        public String convertToString(Object o, Locale locale) {
+            final Enum<?> enumObj = (Enum<?>) o;
+            final String key =
+                enumObj.getDeclaringClass().getSimpleName() + "." +
+                enumObj.name();
+            final String value = Application.get().getResourceSettings()
+                    .getLocalizer().getString(key, null);
+            return Strings.escapeMarkup(value).toString();
+        }
+
+        @Override
+        public Object convertToObject(String s, Locale locale) {
+            return null;
+        }
+    };
+    private static final IConverter convDate = new IConverter() {
+        @Override
+        public String convertToString(Object o, Locale locale) {
+            return FastDateFormat.getInstance("yyyy-MM-dd")
+                .format((Date) o);
+        }
+
+        @Override
+        public Object convertToObject(String s, Locale locale) {
+            return null;
+        }
+    };
+    private static final String CSS_CLASS = "collectionDetails";
 
     /*
      * Actually, we really want the behavior to hide the component on
@@ -73,12 +111,32 @@ public class VirtualCollectionDetailsPage extends BasePage {
             super.cleanup();
             components.clear();
         }
-    }
+    } // class VirtualCollectionDetailsPage.HideIfEmptyBehavior
+    
+    private static class CustomLabel extends Label {
+
+        public CustomLabel(String id) {
+            super(id);
+        }
+
+        @Override
+        public IConverter getConverter(Class<?> type) {
+            if (VirtualCollection.Type.class.isAssignableFrom(type) ||
+                VirtualCollection.Purpose.class.isAssignableFrom(type) ||
+                VirtualCollection.Reproducibility.class.isAssignableFrom(type)) {
+                return convEnum;
+            }
+            if (Date.class.isAssignableFrom(type)) {
+                return convDate;
+            }
+            return super.getConverter(type);
+        }
+    } // class VirtualCollectionDetailsPage.TypeLabel
 
     
     private final HideIfEmptyBehavior hideIfEmpty = new HideIfEmptyBehavior();
 
-
+    
     public VirtualCollectionDetailsPage(final IModel<VirtualCollection> model,
             final Page previousPage) {
         super(new CompoundPropertyModel<VirtualCollection>(model.getObject()));
@@ -97,11 +155,11 @@ public class VirtualCollectionDetailsPage extends BasePage {
                 new Model<String>("General"), CSS_CLASS);
         add(general);
         general.add(new Label("name"));
-        general.add(new Label("type"));
-        general.add(new Label("creationDate"));
+        general.add(new CustomLabel("type"));
+        general.add(new CustomLabel("creationDate"));
         general.add(new MultiLineLabel("description").add(hideIfEmpty));
-        general.add(new Label("purpose").add(hideIfEmpty));
-        general.add(new Label("reproducibility").add(hideIfEmpty));
+        general.add(new CustomLabel("purpose").add(hideIfEmpty));
+        general.add(new CustomLabel("reproducibility").add(hideIfEmpty));
         general.add(new Label("reproducibilityNotice").add(hideIfEmpty));
         final ListView<String> keywords = new ListView<String>("keywords") {
             @Override
@@ -137,18 +195,44 @@ public class VirtualCollectionDetailsPage extends BasePage {
                 final List<Creator> creators = listViewModel.getObject();
                 return new CompoundPropertyModel<Creator>(creators.get(index));
             }
+
+            @Override
+            protected ListItem<Creator> newItem(int index) {
+                final IModel<Creator> model =
+                    getListItemModel(getModel(), index);
+                return new OddEvenListItem<Creator>(index, model) {
+                    @Override
+                    protected void onComponentTag(ComponentTag tag) {
+                        super.onComponentTag(tag);
+                        if (getIndex() == 0) {
+                            tag.append("class", "first", " ");
+                        }
+                    }
+                };
+            }
         });
 
         final Border resources = new AjaxToggleBorder("resourcesBorder",
-                new Model<String>("Resources"), CSS_CLASS);
+                new Model<String>("Resources"), CSS_CLASS + " resources");
         add(resources);
         
         final VirtualCollection vc = model.getObject();
-        final List<IColumn<Resource>> cols = new ArrayList<IColumn<Resource>>();
-        
-        cols.add(new PropertyColumn<Resource>(new Model<String>("Type"), "type"));
-        cols.add(new PropertyColumn<Resource>(new Model<String>("Reference"), "ref"));
+        @SuppressWarnings("rawtypes")
+        final IColumn[] cols = new IColumn[2];
+        cols[0] = new PropertyColumn<Resource>(
+                new Model<String>("Type"), "type") {
+            @Override
+            public void populateItem(Item<ICellPopulator<Resource>> item,
+                    String componentId, IModel<Resource> model) {
+                final Resource.Type type = model.getObject().getType();
+                item.add(new Label(componentId,
+                        convEnum.convertToString(type, getLocale())));
+            }
+        };
+        cols[1] = new PropertyColumn<Resource>(
+                new Model<String>("Reference"), "ref");
 
+        @SuppressWarnings("unchecked")
         final DataTable<Resource> resourcesTable =
             new AjaxFallbackDefaultDataTable<Resource>("resourcesTable",
                     cols,
