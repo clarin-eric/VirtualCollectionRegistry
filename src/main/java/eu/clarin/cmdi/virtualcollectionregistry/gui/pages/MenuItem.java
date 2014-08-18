b5d80c7206d4251677cddacf74b59297b2bf5ffa
Twan Goosen <twan@clarin.eu>
2014-08-18 14:22:07 +0000

    Initial step towards integration of CLARIN style: adapted base page, menu & login, removed some overlapping vcr styling Refs #594

--- src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/pages/MenuItem.java
+++ src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/pages/MenuItem.java
@@ -1,52 +1,55 @@
 package eu.clarin.cmdi.virtualcollectionregistry.gui.pages;
 
+import eu.clarin.cmdi.virtualcollectionregistry.gui.Application;
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.authorization.IAuthorizationStrategy;
 import org.apache.wicket.authorization.strategies.role.annotations.AuthorizeAction;
-import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.util.lang.Classes;
-
-import eu.clarin.cmdi.virtualcollectionregistry.gui.Application;
 
 @SuppressWarnings("serial")
 public class MenuItem<T extends WebPage> extends Panel {
+
     private static final String ENABLE = "ENABLE";
     private final String pageClassName;
 
     public MenuItem(final IModel<String> title, final Class<T> pageClass) {
         super("menuitem");
         this.pageClassName = pageClass.getName();
-        setRenderBodyOnly(true);
-        final Link<T> link = new BookmarkablePageLink<T>("link", pageClass) {
-            @Override
-            protected void onComponentTag(ComponentTag tag) {
-                super.onComponentTag(tag);
-                if (linksTo(getPage())) {
-                    tag.setName("span");
-                    tag.getAttributes().remove("href");
-                    tag.getAttributes().put("class", "active");
-                }
-            }
-        };
+        final Link<T> link = new BookmarkablePageLink<T>("link", pageClass)
+                .setAutoEnable(false);
         link.add(new Label("title", title).setRenderBodyOnly(true));
         add(link);
+
+        add(new AttributeModifier("class", true, new AbstractReadOnlyModel<String>() {
+            @Override
+            public String getObject() {
+                if (pageClass.equals(getPage().getClass())) {
+                    return "active";
+                } else {
+                    return null;
+                }
+            }
+        }));
     }
 
     @Override
     protected void onBeforeRender() {
-        final IAuthorizationStrategy strategy =
-            getApplication().getSecuritySettings()
+        final IAuthorizationStrategy strategy
+                = getApplication().getSecuritySettings()
                 .getAuthorizationStrategy();
         final Class<T> pageClass = getPageClass();
         if (!strategy.isInstantiationAuthorized(pageClass)) {
             boolean visible = true;
-            AuthorizeAction action =
-                pageClass.getAnnotation(AuthorizeAction.class);
+            AuthorizeAction action
+                    = pageClass.getAnnotation(AuthorizeAction.class);
             if ((action != null) && ENABLE.equalsIgnoreCase(action.action())) {
                 final Application app = (Application) getApplication();
                 if (app.hasAnyRole(action.deny())) {
