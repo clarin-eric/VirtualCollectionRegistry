a5beb3838952b354c2e282b7f516d8de07b4b434
Willem Elbers <wjm.elbers@gmail.com>
2017-07-14 14:49:57 +0200

    - Added cite icon based on fontawesome icons

--- src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/citation/CitationPanel.java
+++ src/main/java/eu/clarin/cmdi/virtualcollectionregistry/gui/citation/CitationPanel.java
@@ -16,9 +16,11 @@
  */
 package eu.clarin.cmdi.virtualcollectionregistry.gui.citation;
 
+import eu.clarin.cmdi.virtualcollectionregistry.gui.pages.CreateAndEditVirtualCollectionPage;
 import eu.clarin.cmdi.virtualcollectionregistry.model.VirtualCollection;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -33,12 +35,20 @@ public class CitationPanel extends Panel {
     
     public CitationPanel(String id, final IModel<VirtualCollection> model) {
         super(id);
+        
+        
         AjaxLink citeButton = new AjaxLink( "citeButton", new Model<String>("Cite") ){ 
             @Override
             public void onClick( AjaxRequestTarget target ) {
                 citationDialog.show(target);
             } 
         };
+        citeButton.add(new AttributeAppender("data-toggle", Model.of("tooltip")));
+        citeButton.add(new AttributeAppender("data-placement", Model.of(CreateAndEditVirtualCollectionPage.DEFAULT_TOOLTIP_DATA_PLACEMENT)));
+        citeButton.add(new AttributeAppender("data-html", Model.of("true")));
+        citeButton.add(new AttributeAppender("data-trigger", Model.of("hover")));
+        citeButton.add(new AttributeAppender("title", Model.of("Cite this collection")));
+        
         add(citeButton);
         citationDialog = new CitationDialog("citationDialog", model);
         add(citationDialog);
